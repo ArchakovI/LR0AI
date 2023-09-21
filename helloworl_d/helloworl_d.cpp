3fc494a67111c0b39fda9ea979da5ce2f195fc26
tafych <145649085+ArchakovI@users.noreply.github.com>
2023-09-21 10:24:10 +0300

    LR0
    Код нулевой лабораторной работы

---
+++ helloworl_d/helloworl_d.cpp
@@ -0,0 +1,11 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    cout << "Hello World!" << endl;
+    cout << "Vvedite n" << endl;
+    int n;
+    cin >> n;
+    cout << "n = " << n << endl;
+    return 0;
+}
